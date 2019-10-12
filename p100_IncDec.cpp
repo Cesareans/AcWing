61be90c1772bdb268ce24814f5f931dce6c50d24
Cesare <1357959025@qq.com>
2019-10-12 11:37:04 +0800

    p100 : Medium

---
+++ p100_IncDec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+const int N = 100010;
+int arr[N];
+
+int main(){
+    int n;
+    cin >> n;
+    long neg = 0, pos = 0;
+    for(int i = 0 ; i < n; ++i){
+        cin >> arr[i];
+        if(i){
+            int t = arr[i]-arr[i-1];
+            neg += t < 0 ? -t : 0;
+            pos += t > 0 ? t : 0;
+        }
+    }
+    cout<<max(neg,pos) << endl;
+    cout<<abs(neg-pos) + 1;
+
+    return 0;
+}
