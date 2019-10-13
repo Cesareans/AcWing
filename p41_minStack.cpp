19098c3bac851e3e6bf4fa47ee23bfc848265f9e
Cesare <1357959025@qq.com>
2019-10-13 15:52:54 +0800

    p41 : Easy

--- p41_minStack.cpp
+++ p41_minStack.cpp
@@ -7,12 +7,13 @@ public:
 
     void push(int x) {
         s.push(x);
-        sMin.push(sMin.empty()?x:min(x, sMin.top()));
+        // sMin.push(sMin.empty()?x:min(x, sMin.top())); // 普通做法，最小栈与普通栈一致
+        if(sMin.empty() || sMin.top() >= x) sMin.push(x); // 单调栈做法，仅保存最小值，注意等于时也需要存
     }
 
     void pop() {
+        if(s.top() == sMin.top()) sMin.pop();
         s.pop();
-        sMin.pop();
     }
 
     int top() {
